57c9ca570cb549e8f3f806566a1b75368adceb6e
mtasota <miketasota@gmail.com>
2015-06-19 15:06:51 -0400

    Added Settings using PreferenceActivity; currently just has the default settings options from the sample code and needs changed. (ALSO minimum SDK is now 11)

--- app/src/main/java/org/cmucreatelab/tasota/airprototype/views/activities/AddressListActivity.java
+++ app/src/main/java/org/cmucreatelab/tasota/airprototype/views/activities/AddressListActivity.java
@@ -1,5 +1,6 @@
 package org.cmucreatelab.tasota.airprototype.views.activities;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,11 +31,6 @@ public class AddressListActivity extends ActionBarActivity {
             dialogDelete = new AlertDialogAddressListDelete(this, simpleAddress);
             dialogDelete.getAlertDialog().show();
         }
-    }
-
-
-    public void openSettings() {
-        // TODO open settings
     }
 
 
@@ -98,15 +94,11 @@ public class AddressListActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         switch (id) {
             case R.id.action_settings:
                 Log.d(Constants.LOG_TAG, "onOptionsItemSelected: settings selected.");
-                openSettings();
+                startActivity(new Intent(this, SettingsActivity.class));
                 return true;
             case R.id.action_refresh:
                 Log.d(Constants.LOG_TAG, "onOptionsItemSelected: REFRESH selected.");
@@ -116,9 +108,8 @@ public class AddressListActivity extends ActionBarActivity {
                 Log.d(Constants.LOG_TAG, "onOptionsItemSelected: action bar selected.");
                 openDialogNew("");
                 return true;
-            default:
-                return super.onOptionsItemSelected(item);
         }
+        return super.onOptionsItemSelected(item);
     }
 
 }
