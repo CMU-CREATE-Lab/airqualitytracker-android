1f13d4aa5940ef5dbdd056d534a71fbca7c222ac
mtasota <miketasota@gmail.com>
2015-06-25 15:37:51 -0400

    client-auth: Added an option in settings to launch LoginActivity, which will control setting your ESDR username/passwd from within the app.

---
+++ app/src/main/java/org/cmucreatelab/tasota/airprototype/views/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package org.cmucreatelab.tasota.airprototype.views.activities;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import org.cmucreatelab.tasota.airprototype.R;
+
+public class LoginActivity extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
