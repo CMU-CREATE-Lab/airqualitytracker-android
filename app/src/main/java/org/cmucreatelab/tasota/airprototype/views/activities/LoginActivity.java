bdca7113dd262d6888a832749c47740dc4d5aed0
mtasota <miketasota@gmail.com>
2015-06-29 15:17:23 -0400

    client-auth: Fixed login screen when rotated.

--- app/src/main/java/org/cmucreatelab/tasota/airprototype/views/activities/LoginActivity.java
+++ app/src/main/java/org/cmucreatelab/tasota/airprototype/views/activities/LoginActivity.java
@@ -12,36 +12,63 @@ import org.cmucreatelab.tasota.airprototype.helpers.Constants;
 public class LoginActivity extends ActionBarActivity
         implements View.OnClickListener {
 
-    private boolean loggedIn; // controls the views that you see
+    private boolean loggedIn=false; // controls the views that you see
     private EditText editTextLoginUsername,editTextLoginPassword;
     private TextView textViewLogoutUsername;
 //    private Button buttonLogin,buttonLogout;
 
 
+    protected void display() {
+        if (!loggedIn) {
+            setContentView(R.layout.activity_login);
+
+            editTextLoginUsername = (EditText) findViewById(R.id.editTextLoginUsername);
+            editTextLoginPassword = (EditText) findViewById(R.id.editTextLoginPassword);
+            findViewById(R.id.buttonLogin).setOnClickListener(this);
+        } else {
+            setContentView(R.layout.activity_logout);
+            findViewById(R.id.buttonLogout).setOnClickListener(this);
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        // TODO check if logged in (check SharedPreferences)
+        display();
+    }
 
-        editTextLoginUsername = (EditText)findViewById(R.id.editTextLoginUsername);
-        editTextLoginPassword = (EditText)findViewById(R.id.editTextLoginPassword);
-        findViewById(R.id.buttonLogin).setOnClickListener(this);
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        loggedIn = savedInstanceState.getBoolean("loggedIn");
+        display();
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean("loggedIn",loggedIn);
     }
 
 
     @Override
     public void onClick(View view) {
-        if (view == findViewById(R.id.buttonLogin)) {
+        if (!loggedIn) {
             Log.d(Constants.LOG_TAG, "buttonLogin");
             Log.d(Constants.LOG_TAG, "sent auth with username=" + editTextLoginUsername.getText().toString() + ", passwd=" + editTextLoginPassword.getText().toString());
             // TODO send auth with username,passwd
             //String username=editTextLoginUsername.getText().toString(),password=editTextLoginPassword.getText().toString();
             //GlobalHandler.getInstance(getApplicationContext()).httpRequestHandler.requestEsdrToken(username,password);
-            setContentView(R.layout.activity_logout);
-            findViewById(R.id.buttonLogout).setOnClickListener(this);
-        } else if (view == findViewById(R.id.buttonLogout)) {
+            loggedIn = true;
+            display();
+        } else {
             Log.d(Constants.LOG_TAG, "buttonLogout");
-            setContentView(R.layout.activity_login);
+            loggedIn = false;
+            display();
         }
     }
 
