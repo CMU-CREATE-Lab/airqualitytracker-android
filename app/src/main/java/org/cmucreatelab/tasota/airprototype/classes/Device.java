cfc1bdc27a426127683b881df5a9f5f59a126fc5
mtasota <miketasota@gmail.com>
2015-08-14 09:50:58 -0400

    speck-version2: Added new classes for new class structure (this will replace current structure eventually).

---
+++ app/src/main/java/org/cmucreatelab/tasota/airprototype/classes/Device.java
@@ -0,0 +1,8 @@
+package org.cmucreatelab.tasota.airprototype.classes;
+
+/**
+ * Created by mike on 8/14/15.
+ */
+public class Device extends Feed {
+    // TODO add speck device-specific attributes
+}
